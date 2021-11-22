e0f1e1650099606581cdd81221a50e21f41d0089
Daniel <daniel@dcron.in>
2021-11-21 20:23:27 -0700

    changed foreground and background colors for cells

--- src/TopFiveDestinationList.java
+++ src/TopFiveDestinationList.java
@@ -6,7 +6,7 @@ public class TopFiveDestinationList {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	TopDestinationListFrame topDestinationListFrame = new TopDestinationListFrame();
+                TopDestinationListFrame topDestinationListFrame = new TopDestinationListFrame();
                 topDestinationListFrame.setTitle("Top 5 Destination List");
                 topDestinationListFrame.setVisible(true);
             }
@@ -28,24 +28,39 @@ class TopDestinationListFrame extends JFrame {
 
 
         //Make updates to your top 5 list below. Import the new image files to resources directory.
-        addDestinationNameAndPicture("1. Top Destination (short sentence description)", new ImageIcon(getClass().getResource("/resources/TestImage.jpg")));
-        addDestinationNameAndPicture("2. 2nd Top Destination", new ImageIcon(getClass().getResource("/resources/TestImage.jpg")));
-        addDestinationNameAndPicture("3. 3rd Top Destination", new ImageIcon(getClass().getResource("/resources/TestImage.jpg")));
-        addDestinationNameAndPicture("4. 4th Top Destination", new ImageIcon(getClass().getResource("/resources/TestImage.jpg")));
-        addDestinationNameAndPicture("5. 5th Top Destination", new ImageIcon(getClass().getResource("/resources/TestImage.jpg")));
-        
+        addDestinationNameAndPicture("1. Top Destination (short sentence description)",
+                new ImageIcon(getClass().getResource("/resources/TestImage.jpg")),
+                "https://www.google.com"
+        );
+        addDestinationNameAndPicture("2. 2nd Top Destination",
+                new ImageIcon(getClass().getResource("/resources/TestImage.jpg")),
+                "https://www.google.com"
+        );
+        addDestinationNameAndPicture("3. 3rd Top Destination",
+                new ImageIcon(getClass().getResource("/resources/TestImage.jpg")),
+                "https://www.google.com"
+        );
+        addDestinationNameAndPicture("4. 4th Top Destination",
+                new ImageIcon(getClass().getResource("/resources/TestImage.jpg")),
+                "https://www.google.com"
+        );
+        addDestinationNameAndPicture("5. 5th Top Destination",
+                new ImageIcon(getClass().getResource("/resources/TestImage.jpg")),
+                "https://www.google.com"
+        );
+
         JList list = new JList(listModel);
         JScrollPane scrollPane = new JScrollPane(list);
 
-        TextAndIconListCellRenderer renderer = new TextAndIconListCellRenderer(2);
+        TextAndIconListCellRenderer renderer = new TextAndIconListCellRenderer(10);
 
         list.setCellRenderer(renderer);
 
         getContentPane().add(scrollPane, BorderLayout.CENTER);
     }
 
-    private void addDestinationNameAndPicture(String text, Icon icon) {
-        TextAndIcon tai = new TextAndIcon(text, icon);
+    private void addDestinationNameAndPicture(String text, Icon icon, String url) {
+        TextAndIcon tai = new TextAndIcon(text, icon, url);
         listModel.addElement(tai);
     }
 }
@@ -54,10 +69,12 @@ class TopDestinationListFrame extends JFrame {
 class TextAndIcon {
     private String text;
     private Icon icon;
+    private String url;
 
-    public TextAndIcon(String text, Icon icon) {
+    public TextAndIcon(String text, Icon icon, String url) {
         this.text = text;
         this.icon = icon;
+        this.url = url;
     }
 
     public String getText() {
@@ -74,6 +91,14 @@ class TextAndIcon {
 
     public void setIcon(Icon icon) {
         this.icon = icon;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
 
@@ -97,13 +122,21 @@ class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer {
     }
 
     public Component getListCellRendererComponent(JList list, Object value,
-    int index, boolean isSelected, boolean hasFocus) {
+                                                  int index, boolean isSelected, boolean hasFocus) {
         // The object from the combo box model MUST be a TextAndIcon.
         TextAndIcon tai = (TextAndIcon) value;
 
         // Sets text and icon on 'this' JLabel.
         setText(tai.getText());
         setIcon(tai.getIcon());
+
+        // Sets different foreground and background colors for selected cells
+        list.setSelectionForeground(Color.white);
+        list.setSelectionBackground(Color.darkGray);
+
+        // Sets different foreground and background colors for unselected cells
+        list.setBackground(Color.black);
+        list.setForeground(Color.gray);
 
         if (isSelected) {
             setBackground(list.getSelectionBackground());
@@ -118,7 +151,7 @@ class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer {
         if (hasFocus) {
             outsideBorder = UIManager.getBorder("List.focusCellHighlightBorder");
         } else {
-            outsideBorder = NO_FOCUS_BORDER;
+            outsideBorder = UIManager.getBorder("");
         }
 
         setBorder(BorderFactory.createCompoundBorder(outsideBorder, insideBorder));
@@ -134,10 +167,21 @@ class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer {
     //
     // http://java.sun.com/javase/6/docs/api/javax/swing/DefaultListCellRenderer.html#override
 
-    public void validate() {}
-    public void invalidate() {}
-    public void repaint() {}
-    public void revalidate() {}
-    public void repaint(long tm, int x, int y, int width, int height) {}
-    public void repaint(Rectangle r) {}
+    public void validate() {
+    }
+
+    public void invalidate() {
+    }
+
+    public void repaint() {
+    }
+
+    public void revalidate() {
+    }
+
+    public void repaint(long tm, int x, int y, int width, int height) {
+    }
+
+    public void repaint(Rectangle r) {
+    }
 }
